8386f92f187a838c42f413c4685c6afc94fdeb90
LazyYou2016 <756642763@qq.com>
2018-11-14 10:16:21 +0800

    进行设计“颜色”“按钮”“选项卡”“进度条”“标签”“徽章”通用性变量与样式布局（已完成）

---
+++ dist/js/fileSrc.js
@@ -0,0 +1,28 @@
+﻿var filesSrc = {
+  cssPath: 'dist/css/',
+  jsPath: 'dist/js/',
+  isLayer: 'dist/js/layer/'
+};
+
+//扩展
+var fnExtend = {
+  includFile: function (g, c) {
+    for (var f = "string" == typeof c ? [c] : c, d = 0; d < f.length; d++) {
+      var b = f[d].replace(/^\s|\s$/g, ""),
+        a = b.split("."),
+        e = "css" == a[a.length - 1].toLowerCase(),
+        a = e ? "link" : "script",
+        h = e ? " type='text/css' rel='stylesheet' " : " ",
+        b = (e ? "href" : "src") + "='" + g + b + "'";
+
+      // console.log("<" + a + h + b + "></" + a + ">");
+      0 == document.write("<" + a + h + b + "></" + a + ">")
+    }
+  }
+};
+
+//插入css文件
+fnExtend.includFile(filesSrc.cssPath, ['bootstrap.min.css', 'dataTables.bootstrap.min.css', 'style.css']);
+//插入js文件
+fnExtend.includFile(filesSrc.jsPath, ['jquery.min.js', 'dataTables.bootstrap.min.js', 'common.js']);
+fnExtend.includFile(filesSrc.isLayer, ['layer.js']);
