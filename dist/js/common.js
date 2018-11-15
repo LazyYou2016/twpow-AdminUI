0fc24612faaf8cacc4955d88002bce6a51b3c443
LazyYou2016 <756642763@qq.com>
2018-11-15 17:57:27 +0800

    （1）新增“全局圆角默认变量与自定义圆角”“图标按钮”通用性样式布局（已完成） （2）进行设计“登录页”模板（已完成） （3）完成设计“css与js文件动态加载”模块（已完成）

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,22 +1,21 @@
-function statInputNum(textArea, numItem) {
-  var max = numItem.text(),
-    curLength;
-  //      textArea[0].attr("maxlength", max);
-  curLength = textArea.val().length;
-  //      numItem.text(max - curLength);
-  textArea.on('input propertychange', function () {
-    /*            numItem.text(max - $(this).val().length);
-        if (max - $(this).val().length<0) {
-          layer.msg('字数已超出限制');
-      textArea.val(textArea.val().substr(0, max))
-          numItem.text("0");
-        } */
-    numItem.siblings("span").text($(this).val().length);
+function textAreaCount(textArea) {
+  var max = textArea.find(".textarea-max").text(),
+    textAreaContent = textArea.find("textarea");
+  textAreaContent.on('input propertychange', function () {
+    var textAreaText = $(this).parent("div").find(".wordwrap span");
+    textAreaText.text($(this).val().length);
     if ($(this).val().length > max) {
       layer.msg('字数已超出限制');
-      textArea.val(textArea.val().substr(0, max))
-      numItem.siblings("span").text($(this).val().length);
+      $(this).val($(this).val().substr(0, max))
+      textAreaText.text($(this).val().length);
       return false;
     }
   });
 }
+/* 
+textAreaCount($(".wordCount"));
+在textarea结束标签下插入以下：
+<div class="wordwrap">
+  <span>0</span>/<var class="textarea-max">300</var>
+</div>
+*/
