f0440f0fe32f72f4924928879050a533436b4dbd
BOB <bqby@vip.qq.com>
2018-11-15 18:02:50 +0800

    1.完善单选框和多选框代码和百度上传插件代码 2.整理日期控件代码

--- dist/js/fileSrc.js
+++ dist/js/fileSrc.js
@@ -1,7 +1,8 @@
 ﻿var filesSrc = {
   cssPath: 'dist/css/',
   jsPath: 'dist/js/',
-  isLayer: 'dist/js/layer/'
+  isLayer: 'dist/js/layer/',
+    plugins: 'dist/js/plugins/'
 }
 
 //扩展
@@ -29,3 +30,4 @@ fnExtend.includFile(filesSrc.cssPath, ['bootstrap.min.css', 'style.css'])
 //插入js文件
 fnExtend.includFile(filesSrc.jsPath, ['jquery.min.js', 'bootstrap.min.js', 'dataTables.bootstrap.min.js', 'select2.min.js', 'common.js'])
 fnExtend.includFile(filesSrc.isLayer, ['layer.js'])
+fnExtend.includFile(filesSrc.plugins, ['bootstrap-datetimepicker/bootstrap-datetimepicker.min.js','bootstrap-datetimepicker/bootstrap-datetimepicker.zh-CN.js', 'My97DatePicker/WdatePicker.js'])
