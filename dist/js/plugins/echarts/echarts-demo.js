1f78ede52b7a049a26c7ca4a6f7557251c9cf978
BOB <bqby@vip.qq.com>
2018-11-20 18:54:27 +0800

    百度图表echarts,折线图封装

--- dist/js/plugins/echarts/echarts-demo.js
+++ dist/js/plugins/echarts/echarts-demo.js
@@ -1,5 +1,29 @@
+function test(obj,fun){
+    console.log(obj, fun())
+}
+/**
+ * echarts调色盘颜色
+ * */
+var colorArr = ['#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3']
+/**
+ * 数组是否存在
+ * */
+Array.prototype.contains = function (obj) {
+    // console.log(obj)
+    // console.log(this)
+    var i = this.length;
+    while (i--){
+        if(this[i] === obj){
+            return true;
+        }
+    }
+    // console.log(i)
+    return false
+
+}
 var MyEcharts = {
     EchartsDataFormate: {
+        //整理数据没有分组类型的，适合饼图
         NoGroupFormate: function (data) {
             // category 的数据存储
             var categorys = [];
@@ -10,8 +34,8 @@ var MyEcharts = {
                 categorys.push(data[i].name || '');
                 //定义一个中间变量
                 var temp_data = {
-                    value: data[i].value || 0,
-                    name: data[i].name || ''
+                    name: data[i].name || '',
+                    value: data[i].value || 0
                 }
                 datas.push(temp_data)
             }
@@ -19,60 +43,188 @@ var MyEcharts = {
                 categorys: categorys,
                 data: datas
             }
+        },
+        //整理数据有分组类型的，适合折线图、柱形图（分组，堆积）
+        //数据格式：group：XXX，name：XXX，value：XXX
+        /**
+         * @param data : json数据
+         * @param type : 图表类型
+         * var data1 = [
+         *	  { group:'类型1' , name: '1月', value: 10 },
+         *    { group:'类型2' , name: '1月', value: 15 },
+         *    { group:'类型1' , name: '2月', value: 25 },
+         *    { group:'类型2' , name: '2月', value: 12 },
+         *    { group:'类型1' , name: '3月', value: 22 },
+         *    { group:'类型2' , name: '3月', value: 12 }
+         *    ];
+         */
+        GroupFormate: function (data, type) {
+            //用于存储类型名称
+            var groups = new Array();
+            //用于存储data.name数据
+            var names = new Array();
+            //存储返回series数据(一个或者多个)
+            var series = new Array();
+
+            for(var i = 0; i < data.length;i++) {
+                //判断data[i].group是否存在数租groups中
+                if(!groups.contains(data[i].group)) {
+                    groups.push(data[i].group)
+                    // console.log('s=====>',groups)
+                }
+                //判断name数据是否存在,数组names中
+                if(!names.contains(data[i].name)) {
+                    names.push(data[i].name)
+                    // console.log('names=====>',names)
+                }
+            }
+            //series 遍历分类
+            for (var i =0; i< groups.length; i++) {
+                // 定义一个series中间变量
+                var temp_series = {};
+                // 定义series.data 数据存储
+                var temp_data = [];
+
+                // 遍历所有数据
+                for (var j = 0; j < data.length; j++) {
+                    // console.log(data[j])
+                    for (var k = 0; k < names.length; k++) {
+                        if(groups[i] == data[j].group && names[k] == data[j].name) {
+                            // console.log(data[j].value)
+                            temp_data.push(data[j].value)
+                        }
+                    }
+                }
+                temp_series = {
+                    name: groups[i],
+                    type: type,
+                    data: temp_data
+                }
+                series.push(temp_series)
+            }
+            console.log(series)
+            return {
+                groups: groups,
+                category: names,
+                series:series
+            }
         }
     },
     // 生成图形option
     EchartsOption: {
         /**
          * bar
-         * @param title: 标题
-         * @param subtext: 副标题
          * @param data: json 数据
+         * @param opts  配置
          */
-        bar: function (title,subtext, data) {
-            console.log(data)
-            var datas = MyEcharts.EchartsDataFormate.NoGroupFormate(data);
-            console.log(datas)
-            var arr = []
-            for (var i =0;i < datas.data.length;i++){
-                arr.push(datas.data[i])
+        bar: function (data, opts) {
+            var settings = {
+                title: '', // 标题
+                subtext: '' // 副标题
+            };
+            $.extend(settings,opts);
+            // console.log(data)
+            var datas = MyEcharts.EchartsDataFormate.GroupFormate(data, 'bar');
+
+            option = {
+                title: {
+                    text: settings.title,
+                    subtext: settings.subtext
+                },
+                tooltip: {
+                    show: true,
+                    trigger: 'item',
+                    formatter: "{a}<br/>{b} : {c}"
+                },
+                legend: {
+                    data:datas.groups
+                },
+                xAxis: {
+                    data: datas.category
+                },
+                yAxis: {},
+                series: datas.series
+            };
+            return option
+        },
+        /**
+         * bar
+         * @param data: json 数据
+         * @param opts  配置
+         */
+        line: function (data, opts) {
+            var settings = {
+                title: '', // 标题
+                subtext: '', // 副标题
+                smooth: false // 是否平滑曲线显示
+            };
+            $.extend(settings,opts);
+            // console.log(data)
+            var datas = MyEcharts.EchartsDataFormate.GroupFormate(data, 'line');
+            console.log(datas.series)
+
+            // 是否平滑曲线显示
+            if (settings.smooth) {
+                for (var i = 0; i < datas.series.length ; i++) {
+                    datas.series[i]['smooth']=settings.smooth
+                }
+            }
+
+            // 是否面积图显示
+            if (settings.areaStyle !== undefined) {
+                for (var i = 0; i < datas.series.length ; i++) {
+                    console.log(datas.series[i]['areaStyle']=settings.areaStyle)
+                }
+            }
+
+
+            if (settings.stack !== undefined) {
+                console.log(settings.stack)
+                for (var i = 0; i < datas.series.length ; i++) {
+                    datas.series[i]['stack']=settings.stack
+                }
+
             }
             option = {
                 title: {
-                    text: title,
-                    subtext: subtext
+                    text: settings.title,
+                    subtext: settings.subtext
                 },
-                tooltip: {},
+                tooltip: {
+                    show: true,
+                    trigger: 'axis',
+                    formatter: "{a}<br/>{b} : {c}"
+                },
                 legend: {
-                    data:['销量']
+                    data:datas.groups
                 },
                 xAxis: {
-                    data: datas.categorys
+                    data: datas.category
                 },
                 yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: arr
-                }]
+                series: datas.series,
+                color: colorArr
             };
             return option
         },
         /**
          * 饼图
-         * @param title: 标题
-         * @param subtext: 副标题
          * @param data: json 数据
+         * @param opts 配置
          */
-        pie: function (title, subtext, data) {
+        pie: function (data, opts) {
+            var settings = {
+                title: '', // 标题
+                subtext: '' // 副标题
+            };
+            $.extend(settings,opts);
             // 数据格式
             var datas = MyEcharts.EchartsDataFormate.NoGroupFormate(data);
-            // console.log(datas)
             option = {
                 //标题
                 title: {
-                    text: title || '', //标题
-                    subtext: subtext || '', // 副标题
+                    text: settings.title,
+                    subtext: settings.subtext,
                     x: 'center'
                 },
                 // 提示
@@ -89,11 +241,11 @@ var MyEcharts = {
                 },
                 series: [
                     {
-                        name: title || '',
+                        name: settings.title,
                         type: 'pie',
                         radius: '50%',
                         center: ['50%', '50%'],
-                        data: datas.data,
+                        data: data,
                         itemStyle: {
                             emphasis: {
                                 shadowBlur: 10,
@@ -121,8 +273,18 @@ var MyEcharts = {
             var myChart = echarts.init(container)
 
             myChart.setOption(option);
+            MyEcharts.adaptionWindows(myChart)
             return myChart
         },
 
+    },
+    /**
+     * Echarts自适应屏幕
+     * @param chartName
+     */
+    adaptionWindows: function(chartName) {
+        window.addEventListener('resize',function () {
+            chartName.resize();
+        });
     }
 }
