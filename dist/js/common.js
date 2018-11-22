3db0fe97a8536e397e5f920dba4c02c701d499b3
BOB <bqby@vip.qq.com>
2018-11-22 18:37:17 +0800

    增加日历FullCalendar组件(基本日历、日历json、事件拖放、排班系统)

--- dist/js/common.js
+++ dist/js/common.js
@@ -60,7 +60,7 @@ function textAreaCount(textArea) {
     }
   });
 }
-/* 
+/*
 textAreaCount($(".wordCount"));
 在textarea结束标签下插入以下：
 <div class="wordwrap">
@@ -92,3 +92,40 @@ function tableFull() {
   return winH - 210 + 'px'
 }
 //dataTables E
+/**
+ * 排版系统日历依懒
+ * */
+function uuid(len, radix) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    var uuid = [], i;
+    radix = radix || chars.length;
+    if (len) {
+        // Compact form
+        for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+    } else {
+        // rfc4122, version 4 form
+        var r;
+        // rfc4122 requires these characters
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+        uuid[14] = '4';
+        // Fill in random data.  At i==19 set the high bits of clock sequence as
+        // per rfc4122, sec. 4.1.5
+        for (i = 0; i < 36; i++) {
+            if (!uuid[i]) {
+                r = 0 | Math.random() * 16;
+                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+            }
+        }
+    }
+
+
+    return uuid.join('');
+}
+/* 数字汉字 */
+function Numbers_to_Chinese(num) {
+    var num_name={1:"一",2:"二",3:"三",4:"四",5:"五",6:"六",7:"七",8:"八",9:"九",10:"十"};
+    return num_name[num];
+}
+/**
+ * 排版系统日历依懒 end
+ * */
